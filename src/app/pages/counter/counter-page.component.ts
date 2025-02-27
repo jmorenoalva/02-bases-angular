120b0b5e5bb28b6bcb895bdcf93b8116fbaefe46
Javier Moreno <jamorenoalva@gmail.com>
2025-02-27 17:44:22 -0500

    Calse 39. Zoneless Angular

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 
 
@@ -11,14 +11,33 @@ import { Component, signal } from "@angular/core";
       width: 75px;
     }
   `,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
   counter = 10;
   counterSignal = signal(10);
 
+  constructor(){
+    setInterval(()=>{
+      // this.counter += 1;
+      // this.counterSignal.update((v)=>v+1); //esta señal si se ejecuta y actualiza el valor, pero es necesario el OnPush
+      console.log('Tick');
+    }, 2000);
+  }
+
   increaseBy(value: number) {
-    this.counter += value;
-    this.counterSignal.update((current) => current + value);
+    if (value>0) {
+      this.counter += value;
+      this.counterSignal.update((current) => current + value);
+    }else{
+      if (this.counter > 0) {
+        this.counter += value;
+      }
+
+      if (this.counterSignal() > 0) {
+        this.counterSignal.update((current) => current + value);
+      }
+    }
   }
 
   decreaseBy(value:number){
