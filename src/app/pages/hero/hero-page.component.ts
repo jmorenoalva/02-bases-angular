9444ce21d758d0c640dbfb8910a960d45d82d89a
Javier Moreno <jamorenoalva@gmail.com>
2025-02-28 18:37:27 -0500

    Clase 41. Solucion a la tarea

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -12,18 +12,25 @@ export class HeroPageComponent {
   constructor() {}
 
   getHeroDescription() {
-    return `${this.name} - ${this.age}`;
+    return `${this.name()} - ${this.age()}`;
   }
   changeHero() {
-    this.name.update((v) => 'Spiderman');
-    this.age.update((v) => 22);
+    // this.name.update((v) => 'Spiderman');
+    // this.age.update((v) => 22);
+
+    this.name.set('Spiderman');
+    this.age.set(22);
   }
   resetForm() {
-    this.name.update((v) => 'Ironman');
-    this.age.update((v) => 45);
+    // this.name.update((v) => 'Ironman');
+    // this.age.update((v) => 45);
+
+    this.name.set('Ironman');
+    this.age.set(45);
   }
 
   changeAge(){
-    this.age.update((v)=>60);
+    // this.age.update((v)=>60);
+    this.age.set(60);
   }
 }
