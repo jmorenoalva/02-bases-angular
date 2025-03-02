bfec976624db7ee07796830130d4bfe22bfe8a35
Javier Moreno <jamorenoalva@gmail.com>
2025-03-02 17:01:39 -0500

    Clase 56. Preparacion de una nueva pagina

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,5 +1,4 @@
-import { NgClass } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,27 +7,19 @@ interface Character {
 }
 
 @Component({
-  selector: 'app-dragonball-page',
-  imports: [NgClass],
-  templateUrl: './dragonball-page.component.html',
-  styleUrl: './dragonball-page.component.css',
+  selector: 'app-dragonball-super-page',
+  imports: [],
+  templateUrl: './dragonball-super-page.component.html',
+  styleUrl: './dragonball-super-page.component.css',
 })
-export class DragonballPageComponent {
+export class DragonballSuperPageComponent {
   name = signal('');
   power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    // { id: 2, name: 'Vegeta', power: 8000 },
-    // { id: 3, name: 'Piccolo', power: 3000 },
-    // { id: 4, name: 'Yamcha', power: 500 },
+    { id: 2, name: 'Vegeta', power: 8000 },
   ]);
-
-  // powerClasses= computed(()=> {
-  //   return {
-  //     'text-danger': true,
-  //   }
-  // })
 
   addCharacter() {
     if (!this.name() || !this.power() || this.power() <=0 ) {
