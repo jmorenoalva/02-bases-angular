07bbff6d7c10589c3456443f7b004fcd76528f42
Javier Moreno <jamorenoalva@gmail.com>
2025-03-02 16:53:25 -0500

    Clase 55. Solucion a la tarea

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -11,18 +11,17 @@ interface Character {
   selector: 'app-dragonball-page',
   imports: [NgClass],
   templateUrl: './dragonball-page.component.html',
-  styleUrl: './dragonball-page.component.css'
+  styleUrl: './dragonball-page.component.css',
 })
 export class DragonballPageComponent {
-
   name = signal('Gohan');
   power = signal(100);
 
   characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power:9001},
-    {id: 2, name: 'Vegeta', power:8000},
-    {id: 3, name: 'Piccolo', power:3000},
-    {id: 4, name: 'Yamcha', power:500},
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
 
   // powerClasses= computed(()=> {
@@ -30,4 +29,25 @@ export class DragonballPageComponent {
   //     'text-danger': true,
   //   }
   // })
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <=0 ) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length +1,
+      name: this.name(),
+      power: this.power(),
+    }
+
+    this.characters.update((list) => [...list, newCharacter]);
+
+    this.resetFields()
+  }
+
+  resetFields(){
+    this.name.set('');
+    this.power.set(0);
+  }
 }
