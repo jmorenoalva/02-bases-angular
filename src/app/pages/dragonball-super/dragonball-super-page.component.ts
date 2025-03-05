f7f309b9027c22709525276e27ad46ecf888cbe1
Javier Moreno <jamorenoalva@gmail.com>
2025-03-05 00:31:22 -0500

    Clase 57. Funcion input - Comunicacion entre componentes

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
 
 interface Character {
   id: number;
@@ -8,7 +9,7 @@ interface Character {
 
 @Component({
   selector: 'app-dragonball-super-page',
-  imports: [],
+  imports: [CharacterListComponent],
   templateUrl: './dragonball-super-page.component.html',
   styleUrl: './dragonball-super-page.component.css',
 })
